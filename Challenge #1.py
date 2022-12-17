0f8e69e5d908cde5091f48c43b17bbc9d30b760c
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2022-12-17 22:09:40 +0000

    All kcal data seperated and can be indexed.

--- Challenge #1.py
+++ Challenge #1.py
@@ -1,23 +1,49 @@
-Challenge #1
-
 ##Find the Elf carrying the most Calories. How many total Calories is that Elf carrying?##
 
 FilePath = "D:\Python\Advent of Code 2022\Challenge 1\Caloric Content List.txt"
 
 OpenFile = open(FilePath,"r")
 data = OpenFile.read() ##access data
-
 stringlist = data.split("\n") ##splits each line into list instances
 
+V = 0
+newInt = []
 intList = []
+totalKcals = []
 
-for string in stringlist:
-    is_int = string.isdigit()
-    if is_int:
-        newInt = int(string)
-        intList.append(newInt)
-    else is_int:
-        newSpace = str("\n")
-        intList.append(newSpace)
+for string in stringlist:               #for any indexed item in the list of strings "stringlist"
+    is_int = string.isdigit()           #is_int produces a boolean value depending on data input
+    if string == '':                    #if string is a blank space, this is appended to the new list "intList"
+        intList.append(string)          
+    if is_int == True:                  #if is_int is true, convert the info held in the variable "string" into an interger
+        newInt = int(string)            
+        intList.append(newInt)          #append new data type into the list "intList"
 
-print(intList) ##All data is missing the blank, seperator between what each elf is carrying.
+print(intList)                          #sanity check
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
