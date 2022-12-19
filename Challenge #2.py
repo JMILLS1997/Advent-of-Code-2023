b6d60b9fefacb62bd144ac27dece3543be2b419f
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2022-12-19 20:34:34 +0000

    Update Challenge #2.py
    General data set up and a short description of my approach in comments at bottom of script.

--- Challenge #2.py
+++ Challenge #2.py
@@ -5,14 +5,14 @@
 FilePath = "C:\\Users\\James Mills\\Documents\\GitHub\\Advent-of-Code-2022\\inputs\\input 2.txt"
 
 OpenFile = open(FilePath,"r")
-data = OpenFile.read()                  #access data
+data = OpenFile.readlines()                  #access data
 
-#points per type of hand played
+#opponent hand types and point values
 A = 1                   #Rock
 B = 2                   #Paper
 C = 3                   #Scissors
 
-#response hand types
+#player(you) hand types and point values
 X = 1                   #Rock
 Y = 2                   #Paper
 Z = 3                   #Scissors
@@ -22,4 +22,14 @@ win = 6
 draw = 3
 loose = 0
 
+i = 0
 
+for str in data:                     #for any indexed item line read from "data.txt" file
+    
+          
+
+
+#run through each line in a for loop (will need for loop inside of a for loop)
+#run through strings in the line to determine which of the above characters are being played
+#calculate outcome for each line individually
+#calculate sum total
