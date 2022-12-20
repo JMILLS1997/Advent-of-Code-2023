bee31b9a025aa4971fb342845cf35efd0fd05aa9
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2022-12-20 18:14:32 +0000

    Update Challenge #2.py

--- Challenge #2.py
+++ Challenge #2.py
@@ -7,29 +7,45 @@ FilePath = "C:\\Users\\James Mills\\Documents\\GitHub\\Advent-of-Code-2022\\inpu
 OpenFile = open(FilePath,"r")
 data = OpenFile.readlines()                  #access data
 
-#opponent hand types and point values
-A = 1                   #Rock
-B = 2                   #Paper
-C = 3                   #Scissors
-
-#player(you) hand types and point values
-X = 1                   #Rock
-Y = 2                   #Paper
-Z = 3                   #Scissors
+#score dictionary
+#A = X = 1               #Rock
+#B = Y = 2               #Paper
+#C = Z = 3               #Scissors
+DictScr1 = {'A': 1, 'B': 2, 'C': 3}
+DictScr2 = {'X': 1, 'Y': 2, 'Z': 3}
 
 #scores for outcome types
-win = 6
-draw = 3
-loose = 0
+#win = 6
+#draw = 3
+#loose = 0
+DictWin = {'A Z': 6, 'B X': 6, 'C Y': 6}
+DictDrw = {'A X': 3, 'B Y': 3, 'C Z': 3}
+DictLse = {'A Y': 0, 'B Z': 0, 'C X': 0}
 
-i = 0
+#player scores var
+playerscr = 0
+opponentscr = 0
+gamescr = 0
 
-for str in data:                     #for any indexed item line read from "data.txt" file
+for lines in data:
+    for eachkey in DictWin.keys():
+        if eachkey in lines:
+            print('win')
+        else:
+            pass
     
-          
+    for eachkey in DictWin.keys():
+            if eachkey in lines:
+                print('draw')
+            else:
+                pass
+    
+    for eachkey in DictWin.keys():
+            if eachkey in lines:
+                print('loose')
+            else:
+                pass
+    
 
 
-#run through each line in a for loop (will need for loop inside of a for loop)
-#run through strings in the line to determine which of the above characters are being played
-#calculate outcome for each line individually
-#calculate sum total
+
