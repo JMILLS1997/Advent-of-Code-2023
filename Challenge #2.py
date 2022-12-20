a7667c5930be4b2adc0912f770448f91b954277f
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2022-12-20 19:45:23 +0000

    Struggling compare input list and ref. dict.

--- Challenge #2.py
+++ Challenge #2.py
@@ -5,47 +5,35 @@
 FilePath = "C:\\Users\\James Mills\\Documents\\GitHub\\Advent-of-Code-2022\\inputs\\input 2.txt"
 
 OpenFile = open(FilePath,"r")
-data = OpenFile.readlines()                  #access data
+data = OpenFile.readlines()                 #access data
+adjData = [x[:-1] for x in data]            #removes rightmost character from list data for every instance in "data" list
+#print(adjData)
 
 #score dictionary
 #A = X = 1               #Rock
 #B = Y = 2               #Paper
 #C = Z = 3               #Scissors
-DictScr1 = {'A': 1, 'B': 2, 'C': 3}
-DictScr2 = {'X': 1, 'Y': 2, 'Z': 3}
+DictOpp = {'A': 1, 'B': 2, 'C': 3}
+DictPlr = {'X': 1, 'Y': 2, 'Z': 3}
 
 #scores for outcome types
 #win = 6
 #draw = 3
 #loose = 0
-DictWin = {'A Z': 6, 'B X': 6, 'C Y': 6}
-DictDrw = {'A X': 3, 'B Y': 3, 'C Z': 3}
-DictLse = {'A Y': 0, 'B Z': 0, 'C X': 0}
+DictOC = {'A Z': 6, 'B X': 6, 'C Y': 6, 'A X': 3, 'B Y': 3, 'C Z': 3, 'A Y': 0, 'B Z': 0, 'C X': 0}
+
 
 #player scores var
 playerscr = 0
 opponentscr = 0
 gamescr = 0
 
-for lines in data:
-    for eachkey in DictWin.keys():
-        if eachkey in lines:
-            print('win')
+
+for i in adjData:     
+    for eachkey in DictOC.keys():
+        if i == eachkey:
+            print(DictOC.values())
+           
         else:
-            pass
+            print(eachkey)
     
-    for eachkey in DictWin.keys():
-            if eachkey in lines:
-                print('draw')
-            else:
-                pass
-    
-    for eachkey in DictWin.keys():
-            if eachkey in lines:
-                print('loose')
-            else:
-                pass
-    
-
-
-
