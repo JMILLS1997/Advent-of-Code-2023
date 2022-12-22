cc29b297dd5b82e87e3ce1b3133bf14d767fde2f
JMILLS1997 <120510403+JMILLS1997@users.noreply.github.com>
2022-12-22 18:38:53 +0000

    prints game outcomes and player hand.

--- Challenge #2.py
+++ Challenge #2.py
@@ -12,24 +12,30 @@ adjData = [x[:-1] for x in data]            #removes rightmost character from li
 #A = X = 1               #Rock
 #B = Y = 2               #Paper
 #C = Z = 3               #Scissors
-DictOpp = {'A': 1, 'B': 2, 'C': 3}
-DictPlr = {'X': 1, 'Y': 2, 'Z': 3}
+DictPlrHnd = {'X': 1, 'Y': 2, 'Z': 3}
 
 #scores for outcome types
 #win = 6
 #draw = 3
 #loose = 0
-DictOC = {'A Z': 6, 'B X': 6, 'C Y': 6, 'A X': 3, 'B Y': 3, 'C Z': 3, 'A Y': 0, 'B Z': 0, 'C X': 0}
+DictOC = {'A Z': 0, 'B X': 0, 'C Y': 0, 'A X': 3, 'B Y': 3, 'C Z': 3, 'A Y': 6, 'B Z': 6, 'C X': 6}
 
 #player scores var
-playerscr = 0
-opponentscr = 0
-gamescr = 0
+playerscr = []
 
 for str in adjData:
-    for key, value in DictOC.items():
-        if key == str:
-            print(value)
+    for key1, value1 in DictOC.items():
+        if key1 == str:
+            print(key1)
+            for key2, value2 in DictPlrHnd.items():
+                if key1[2] == key2:
+                    print(key2)
+            else:
+                pass
             pass
         else:
             pass
+
+#ADD values to list, do same thing for the DictPlr dict and add those values to a list.
+#Will need to compare hand type to game outcome to determin which hand type occurs in the game outcome ('Y' found in 'A Y')
+#simple character comparason for the two strings, one at str[0] and the other at str[2]
